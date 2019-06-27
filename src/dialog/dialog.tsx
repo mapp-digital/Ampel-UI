2fa48c454790cb3895670e19690d67860ae696d9
mostafabalata <mostafa.balata@gmail.com>
2019-06-27 13:57:55 +0200

    Make use of the subscription and wrap the modal with HTMLNode element

--- src/dialog/dialog.tsx
+++ src/dialog/dialog.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import { Button } from '@ampel-ui/button';
+import { onOuterClick } from '@ampel-ui/subscriptions';
 
 const KEY_ESCAPE = 27;
 
@@ -16,70 +17,71 @@ interface Props {
 
 class Dialog extends React.Component<Props, {}> {
     private dialog = React.createRef<HTMLDivElement>();
+    private node: Node;
+    private dispose: () => void;
 
     constructor(props: Props) {
         super(props);
 
         this.onKeyupHandler = this.onKeyupHandler.bind(this);
-
-        this.onClickOutsideHandler = this.onClickOutsideHandler.bind(this);
-
+        this.setNode = this.setNode.bind(this);
         this.focus = this.focus.bind(this);
     }
 
     public componentDidMount() {
         this.focus();
-        document.addEventListener('click', this.onClickOutsideHandler, false);
+        this.dispose = onOuterClick(this.node.firstChild as Node, this.props.onCancel)
+    }
+
+    public componentWillUnmount() {
+        this.dispose();
     }
 
     public render() {
         return (
-            <div
-                id={this.props.id}
-                className="dialog"
-                ref={this.dialog}
-                onKeyUp={this.onKeyupHandler}
-                tabIndex={-1}
-                role="dialog"
-                data-qa={`dialog-${this.props.id}`}
-            >
-                <div className="dialog-content">
-                    <div className="dialog-content-header">
-                        <h3 className="dialog-title">{this.props.title}</h3>
-                    </div>
-                    <div className="dialog-content-body">{this.props.content}</div>
-                    <div className="dialog-content-footer">
-                        <Button
-                            id="cancel"
-                            text={this.props.btnCancelText}
-                            className="btn btn-secondary"
-                            onClick={this.props.onCancel}
-                        />
-                        <Button
-                            id="success"
-                            text={this.props.btnConfirmText}
-                            className="btn btn-primary"
-                            onClick={this.props.onConfirm}
-                        />
+            <div ref={this.setNode} >
+                <div
+                    id={this.props.id}
+                    className="dialog"
+                    ref={this.dialog}
+                    onKeyUp={this.onKeyupHandler}
+                    tabIndex={-1}
+                    role="dialog"
+                    data-qa={`dialog-${this.props.id}`}
+                >
+                    <div className="dialog-content">
+                        <div className="dialog-content-header">
+                            <h3 className="dialog-title">{this.props.title}</h3>
+                        </div>
+                        <div className="dialog-content-body">{this.props.content}</div>
+                        <div className="dialog-content-footer">
+                            <Button
+                                id="cancel"
+                                text={this.props.btnCancelText}
+                                className="btn btn-secondary"
+                                onClick={this.props.onCancel}
+                            />
+                            <Button
+                                id="success"
+                                text={this.props.btnConfirmText}
+                                className="btn btn-primary"
+                                onClick={this.props.onConfirm}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
         );
     }
 
-    private onClickOutsideHandler(e: MouseEvent) {
-        const target: HTMLElement = (e.target as HTMLElement)
-        if (this.dialog.current!.contains(target) && !this.dialog.current!.isEqualNode(target)) {
-            return;
-        }
-        document.removeEventListener('click', this.onClickOutsideHandler, false);
-        this.props.onCancel();
-    }
-
     private onKeyupHandler(event: React.KeyboardEvent) {
         if (event.keyCode === KEY_ESCAPE) {
             this.props.onCancel();
         }
+    }
+
+    private setNode(node: any) {
+        this.node = node;
     }
 
     private focus() {
--- src/subscriptions/index.ts
+++ src/subscriptions/index.ts
@@ -1,8 +1,10 @@
 const onOuterClick = (node: Node, callback: () => void): (() => void) => {
     const listener = (event: MouseEvent) => {
-        if (!node.contains(event.target as Node)) {
-            callback();
+        const target: Node = event.target as Node;
+        if (node.contains(target) && !node.isEqualNode(target)) {
+            return;
         }
+        callback();
     };
     document.addEventListener('mousedown', listener, false);
     return () => document.removeEventListener('mousedown', listener, false);
