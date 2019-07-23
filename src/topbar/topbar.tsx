a7c4cc9be0085928baed7f3cd150525e3052d555
Belmin Bedak <belmoo996@hotmail.com>
2019-07-23 14:55:03 +0200

    fix failing build

--- src/topbar/topbar.tsx
+++ src/topbar/topbar.tsx
@@ -24,8 +24,13 @@ class Topbar<T extends Item> extends React.Component<Props<T>> {
                 </div>
                 <div className="right">
                     {this.props.icons &&
-                        this.props.icons.map((icon, index) =>
-                            <div className={`topbar-icon ${this.props.selectedItemWhen(icon) ? SELECTED_CLASS : ''}`} role="link" key={index} onClick={icon.onClick}>
+                        this.props.icons.map((icon, index) => (
+                            <div
+                                className={`topbar-icon ${this.props.selectedItemWhen(icon) ? SELECTED_CLASS : ''}`}
+                                role="link"
+                                key={index}
+                                onClick={icon.onClick}
+                            >
                                 {icon.label && <span className="badge">{icon.label}</span>}
                                 <span className={icon.classes} />
                             </div>
