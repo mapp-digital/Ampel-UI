af206fc43e5fc9771efca387dbcf5c4e3ea292b5
Mike x057 <mike@x057.com>
2020-10-29 11:15:45 +0100

    ANALYTICS-2006/ANALYTICS-2163: Converts PasswordInput to regular component with state

--- src/input/password-input.tsx
+++ src/input/password-input.tsx
@@ -1,3 +1,4 @@
+import { isFunction, isNil } from 'lodash';
 import * as React from 'react';
 
 interface Props {
@@ -15,51 +16,85 @@ interface Props {
     autoFocus?: boolean;
     passwordVisible?: boolean;
     enablePasswordToggle?: boolean;
-    onPasswordToggle?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    onPasswordToggle?: (value: boolean) => void;
 }
 
-const PasswordInput: React.FunctionComponent<Props> = (props) => {
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => props.onChange(event.target.value);
-    // React assumes inputs with null/undefined value are uncontrolled and will throw warnings when changing the value.
-    const value = props.value !== undefined || props.value !== null ? props.value : '';
-    return (
-        <div
-            className={`password-input-component ${props.enablePasswordToggle ? 'password-toggle-visible' : ''}`}
-            onMouseEnter={props.onMouseEnter}
-            onMouseLeave={props.onMouseLeave}
-        >
-            {props.prepend && (
-                <span className="input-prepend" data-qa="input--prepend">
-                    {props.prepend}
-                </span>
-            )}
-            <input
-                id={props.id}
-                type={props.passwordVisible ? 'text' : 'password'}
-                value={value}
-                onBlur={props.onBlur}
-                onChange={onChange}
-                data-qa={`password-input--component-${props.id}`}
-                className={`form-control ${props.className || ''}`}
-                placeholder={props.placeholder}
-                disabled={props.disabled}
-                autoFocus={props.autoFocus}
-            />
-            {props.enablePasswordToggle && (
-                <button
-                    className={`password-toggle btn btn-secondary ${props.passwordVisible ? 'hide' : 'show'}`}
-                    type="button"
-                    onClick={props.onPasswordToggle}
-                    data-qa={`password--toggle`}
+interface State {
+    value: string;
+    passwordVisible: boolean;
+}
+
+class PasswordInput extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        const value = isNil(props.value) ? '' : props.value;
+        this.state = {
+            value,
+            passwordVisible: Boolean(props.passwordVisible),
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onPasswordToggle = this.onPasswordToggle.bind(this);
+    }
+
+    public render() {
+        return (
+            <div
+                className={`password-input-component ${
+                    this.props.enablePasswordToggle ? 'password-toggle-visible' : ''
+                }`}
+                onMouseEnter={this.props.onMouseEnter}
+                onMouseLeave={this.props.onMouseLeave}
+            >
+                {this.props.prepend && (
+                    <span className="input-prepend" data-qa="input--prepend">
+                        {this.props.prepend}
+                    </span>
+                )}
+                <input
+                    id={this.props.id}
+                    type={this.state.passwordVisible ? 'text' : 'password'}
+                    value={this.state.value}
+                    onBlur={this.props.onBlur}
+                    onChange={this.onChange}
+                    data-qa={`password-input--component-${this.props.id}`}
+                    className={`form-control ${this.props.className || ''}`}
+                    placeholder={this.props.placeholder}
+                    disabled={this.props.disabled}
+                    autoFocus={this.props.autoFocus}
                 />
-            )}
-            {props.append && (
-                <span className="input-append" data-qa="input--append">
-                    {props.append}
-                </span>
-            )}
-        </div>
-    );
-};
+                {this.props.enablePasswordToggle && (
+                    <button
+                        type="button"
+                        disabled={this.props.disabled}
+                        className={`password-toggle btn btn-secondary ${this.state.passwordVisible ? 'show' : 'hide'}`}
+                        onClick={this.onPasswordToggle}
+                        data-qa={`password--toggle`}
+                    />
+                )}
+                {this.props.append && (
+                    <span className="input-append" data-qa="input--append">
+                        {this.props.append}
+                    </span>
+                )}
+            </div>
+        );
+    }
+
+    private onChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const value = event.target.value;
+        this.setState({ value });
+        this.props.onChange(value);
+    }
+
+    private onPasswordToggle(event: React.MouseEvent<HTMLButtonElement>) {
+        const passwordVisible = !this.state.passwordVisible;
+        this.setState({ passwordVisible });
+
+        if (isFunction(this.props.onPasswordToggle)) {
+            this.props.onPasswordToggle(passwordVisible);
+        }
+    }
+}
 
 export { PasswordInput };
